d8d211187e87d7a69af5caad7963c82ff3b5e11e
Andrés Felipe Chaparro <34499769+FelipeSpirit@users.noreply.github.com>
2019-05-27 22:38:19 -0500

    Update test_func.js

--- test_func.js
+++ test_func.js
@@ -1,8 +1,8 @@
-function calculateEvacuation(diameter, holeDiameter, height) {
+async function calculateEvacuation(diameter, holeDiameter, height) {
 	var time=100;
 	while(time>0){
 		window.setTimeout(document.getElementById("demo").innerHTML=time,1000);
-		time--;
+		await time--;
 		sleep(1);
 	}
 }
