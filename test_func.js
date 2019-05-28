0854d8c5b063acc44c7bc093ee5810970a261f41
Andrés Felipe Chaparro <34499769+FelipeSpirit@users.noreply.github.com>
2019-05-27 21:57:14 -0500

    Update test_func.js

--- test_func.js
+++ test_func.js
@@ -1,5 +1,15 @@
 function calculateEvacuation(diameter, holeDiameter, height) {
-	document.getElementById("demo").innerHTML=Math.pow(((diameter / 100) / (holeDiameter / 100)), 2)
-				* Math.sqrt(2 * (height / 100) / 9.8);
+	var time=Math.pow(((diameter / 100) / (holeDiameter / 100)), 2) * Math.sqrt(2 * (height / 100) / 9.8);
+	document.getElementById("demo").innerHTML=t;
+	while(time>0){
+		window.setTimeout(document.write(calculateHeight(time, diameter, holeDiameter)),1);
+		t-=0.001;
+	}
+	
 		
+}
+
+function calculateHeight(time, diameter, holeDiameter) {
+		return (9.8 * Math.pow(time, 2) * Math.pow(holeDiameter / 100, 4))
+				/ (2 * Math.pow(diameter / 100, 4));
 }
