44f11931d193512f880a044b684c3f272a2b1939
Andrés Felipe Chaparro <34499769+FelipeSpirit@users.noreply.github.com>
2019-05-27 22:15:22 -0500

    Update test_func.js

--- test_func.js
+++ test_func.js
@@ -1,9 +1,9 @@
 function calculateEvacuation(diameter, holeDiameter, height) {
 	var time=Math.pow(((diameter / 100) / (holeDiameter / 100)), 2) * Math.sqrt(2 * (height / 100) / 9.8);
-	while(time>0){
+	do{
 		window.setTimeout(document.getElementById("demo").innerHTML=time,1);
 		time-=1;
-	}
+	}while(time>0);
 	
 		
 }
