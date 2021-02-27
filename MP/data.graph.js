00266f9bf2eb91795ceece3ea0e7ba8707e6b953
AFelipeCR <andres.chaparro06@uptc.edu.co>
2021-02-26 19:27:52 -0500

    full

---
+++ MP/data.graph.js
@@ -0,0 +1,114 @@
+var firstPlaces = [
+	{u_id:1, times: 0},
+	{u_id:2, times: 0},
+	{u_id:3, times: 0},
+	{u_id:4, times: 0}
+];
+
+var secondPlaces = [
+	{u_id:1, times: 0},
+	{u_id:2, times: 0},
+	{u_id:3, times: 0},
+	{u_id:4, times: 0}
+];
+
+var thirdPlaces = [
+	{u_id:1, times: 0},
+	{u_id:2, times: 0},
+	{u_id:3, times: 0},
+	{u_id:4, times: 0}
+];
+
+var fourthPlaces = [
+	{u_id:1, times: 0},
+	{u_id:2, times: 0},
+	{u_id:3, times: 0},
+	{u_id:4, times: 0}
+];
+
+readData('data.json').then(data=>{
+	const tableData = document.getElementById('table-data');
+	let i = 0;
+	data.plays.forEach(p=>{
+		firstPlaces.find(f=> f.u_id == p.first).times++;
+		secondPlaces.find(f=> f.u_id == p.second).times++;
+		thirdPlaces.find(f=> f.u_id == p.third).times++;
+		fourthPlaces.find(f=> f.u_id == p.fourth).times++;
+
+		i++;
+		tableData.appendChild(createDataRow(i, p, data.users));
+	});
+
+	createChart('chart1', 'Primer', firstPlaces, toNames(data.users), data.colors);
+	createChart('chart2', 'Segundo', secondPlaces, toNames(data.users), data.colors);
+	createChart('chart3', 'Tercer', thirdPlaces, toNames(data.users), data.colors);
+	createChart('chart4', 'Cuarto', fourthPlaces, toNames(data.users), data.colors);
+})
+
+function createChart(canvas, place, places, users, colors){
+	new Chart(document.getElementById(canvas).getContext('2d'), {
+	    type: 'pie',
+	    data: {
+	    	datasets: [{
+				data: toDataset(places),
+				backgroundColor: colors,
+				label: place + ' lugar'
+			}],
+			labels: users
+	    },
+	    options: {
+	    	responsive: true
+		}
+	});
+}
+
+function createDataRow(i, play, users){
+	let row = document.createElement('tr');
+	let pp = document.createElement('td');
+	pp.className = "text-center";
+	pp.innerHTML = i;
+	let date = document.createElement('td');
+	date.className = "text-center"
+	date.innerHTML = play.date;
+
+	let first = document.createElement('td');
+	first.className = "text-center"
+	first.innerHTML = users.find(u=>u.id == play.first).name;
+
+	let second = document.createElement('td');
+	second.className = "text-center"
+	second.innerHTML = users.find(u=>u.id == play.second).name;
+
+	let third = document.createElement('td');
+	third.className = "text-center"
+	third.innerHTML = users.find(u=>u.id == play.third).name;
+
+	let fourth = document.createElement('td');
+	fourth.className = "text-center"
+	fourth.innerHTML = users.find(u=>u.id == play.fourth).name;
+	row.appendChild(pp);
+	row.appendChild(date);
+	row.appendChild(first);
+	row.appendChild(second);
+	row.appendChild(third);
+	row.appendChild(fourth);
+	return row;
+}
+
+function toDataset(places){
+	return [
+		places[0].times,
+		places[1].times,
+		places[2].times,
+		places[3].times
+	];
+}
+
+function toNames(users){
+	return [
+		users[0].name,
+		users[1].name,
+		users[2].name,
+		users[3].name
+	];
+}
---
+++ MP/prob/data.random.js
@@ -0,0 +1,88 @@
+var firstRandom = [];
+var secondRandom = [];
+var thirdRandom = [];
+var fourthRandom = [];
+
+readData('../data.json').then(data=>{
+	const tableData = document.getElementById('table-data');
+	let i = 0;
+	data.plays.forEach(p=>{
+		firstRandom.push(getUserName(data.users, p.first))
+		secondRandom.push(getUserName(data.users, p.second))
+		thirdRandom.push(getUserName(data.users, p.third))
+		fourthRandom.push(getUserName(data.users, p.fourth))
+	});
+
+	thirdRandom.push("Caulimón")
+	fourthRandom.push("Samheer")
+})
+
+function getUserName(users, id){
+	return users.find(u=>u.id == id).name;
+}
+
+function getRandomName(rand) {
+    return rand[Math.floor(Math.random() * rand.length)];
+}
+
+async function randomize(){
+	let fst, snd, trd,fth;
+	let el1=document.getElementById('1-place');
+	let el2=document.getElementById('2-place');
+	let el3=document.getElementById('3-place');
+	let el4=document.getElementById('4-place');
+	let v = 10;
+	let s = 2000;
+	let fsize = 1;
+
+	do{
+		await sleep(s);
+		fst = el1.innerHTML = getRandomName(firstRandom)
+		el1.style.fontSize = fsize + "rem";
+		fsize+=.4;
+		v--;
+		s /= 1.4;
+	}while(v > 0);
+	let maxsize = fsize;
+	console.log(maxsize)
+
+	fsize = 1;
+	do{
+		await sleep(s);
+		snd = el2.innerHTML = getRandomName(secondRandom)
+		el2.style.fontSize = fsize + "rem";
+		fsize+=.4;
+		s /= 1.4;
+	}while(fst == snd);
+	console.log(maxsize, fsize)
+	if(maxsize > fsize)
+		el2.style.fontSize = maxsize + "rem";
+	s= 2000;
+	fsize = 1;
+	do{
+		await sleep(s);
+		trd = el3.innerHTML = getRandomName(thirdRandom)
+		el3.style.fontSize = fsize + "rem";
+		fsize+=.4;
+		s /= 1.4;
+	}while(fst == trd || snd == trd);
+
+	if(maxsize > fsize)
+		el3.style.fontSize = maxsize + "rem";
+
+	fsize = 1;
+	do{
+		await sleep(s);
+		fth = el4.innerHTML = getRandomName(fourthRandom)
+		el4.style.fontSize = fsize + "rem";
+		fsize+=.4;
+		s /= 1.4;
+	}while(fst == fth || snd == fth || trd == fth);
+
+	if(maxsize > fsize)
+		el4.style.fontSize = maxsize + "rem";
+}
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
